08f1d44917d0323b75e904834b418da0c743ac07
Varshini Manda <varshini.fsj123@gmail.com>
2024-12-09 22:10:27 -0800

    feat: add skills to portfolio

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -46,7 +46,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm VM `}<span className="wrap">{text}</span></h1>
+                        <h1>{`Hi I'm Varshini Manda `}<span className="wrap">{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         <button onClick={() => console.log('clicked')}>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
---
+++ src/components/Skills.js
@@ -0,0 +1,66 @@
+import { Container, Col, Row } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import meter1 from "../assets/images/meter1.svg";
+import meter2 from "../assets/images/meter2.svg";
+import meter3 from "../assets/images/meter3.svg";
+import colorSharp from "../assets/images/color-sharp.png";
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+    return (
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="skill-box">
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Full Stack</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter3} alt="Image" />
+                                    <h5>Amazon Web Services</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Backend</h5>
+                                </div>
+                            </Carousel>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp} />
+        </section>
+    )
+
+}
